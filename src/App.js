cdaf98922c4b5c65293a17eecdc7ce5aa244bdc3
LaetitiaStu <129389279+LaetitiaStu@users.noreply.github.com>
2023-06-12 15:18:03 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./img/book-logo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="Book-logo img-fluid" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
