1254da6ef9117c59641b6b91ea2eecd804ddef0a
LaetitiaStu <129389279+LaetitiaStu@users.noreply.github.com>
2023-06-14 00:31:54 +0200

    Rendered the meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dicitionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data);
+    setResults(response.data[0]);
   }
 
   function search(event) {
@@ -26,6 +28,7 @@ export default function Dictionary() {
         <input type="search" onChange={keywordChange} />
         <input type="submit" value="Search" />
       </form>
+      <Results results={results} />
     </div>
   );
 }
