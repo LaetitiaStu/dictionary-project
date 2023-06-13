745c32d725d559fd57d4d65a004c58706b78eb3c
LaetitiaStu <129389279+LaetitiaStu@users.noreply.github.com>
2023-06-13 19:58:06 +0200

    SearchEngine HTML

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import logo from "./img/book-logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="Book-logo img-fluid" alt="logo" />
-        <h1>Dictionary</h1>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="Book-logo img-fluid" alt="logo" />
+          <h1>Dictionary</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Laetitia Stuchtey</footer>
+      </div>
     </div>
   );
 }
