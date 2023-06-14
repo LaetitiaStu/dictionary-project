2d522d048729ee9499aa4ff0690167b76b2ad61e
LaetitiaStu <129389279+LaetitiaStu@users.noreply.github.com>
2023-06-14 13:03:26 +0200

    Integrated phonetics

---
+++ src/Phonetic.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactAudioPlayer from "react-audio-player";
+
+export default function Phonetic(props) {
+  return (
+    <div className="Phonetic">
+      {props.phonetic.text}
+      <br />
+      <ReactAudioPlayer
+        src={props.phonetic.audio}
+        autoPlay={false}
+        controls={true}
+      />
+    </div>
+  );
+}
